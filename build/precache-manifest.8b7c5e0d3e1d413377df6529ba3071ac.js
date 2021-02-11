8c82f4bff1d086f847312a4ceac268c563f25063
vicsenpai <victorjosue@estudiantec.cr>
2021-02-11 12:35:06 -0600

    🔧 UPDATE: Publish in github pages

---
+++ build/precache-manifest.8b7c5e0d3e1d413377df6529ba3071ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0da3627c491b127cd399",
+    "url": "/nfatodfa/static/js/runtime~main.52043eba.js"
+  },
+  {
+    "revision": "483b5269c2992fd6a78d",
+    "url": "/nfatodfa/static/js/main.b3d5843f.chunk.js"
+  },
+  {
+    "revision": "b55b5fdec4223260cc7a",
+    "url": "/nfatodfa/static/js/2.cc1eadf8.chunk.js"
+  },
+  {
+    "revision": "483b5269c2992fd6a78d",
+    "url": "/nfatodfa/static/css/main.cddf0076.chunk.css"
+  },
+  {
+    "revision": "73d2240aa21fe8f389fee143673c17ec",
+    "url": "/nfatodfa/index.html"
+  }
+];
